20829c42e70c436356e78d9160fd9d6b87d9c446
JoisFe <rhsnqk@gmail.com>
2023-08-23 21:59:07 +0900

    sonarqube_jenkins_prac project start

---
+++ src/main/java/com/example/sonarqube_jenkins_prac/SonarqubeJenkinsPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarqube_jenkins_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeJenkinsPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeJenkinsPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sonarqube_jenkins_prac/SonarqubeJenkinsPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarqube_jenkins_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeJenkinsPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
